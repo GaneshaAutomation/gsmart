d8a8e4f4ffa1acef52fdb82a8117633f656e02bb
Kalpra621 <kalpra621@gmail.com>
2024-01-29 21:05:27 -0500

    GSM-124 HeadValidation updated.

--- gsmart-test-automation/src/test/java/com/gsmart/test/automation/landing/HeaderValidationTest.java
+++ gsmart-test-automation/src/test/java/com/gsmart/test/automation/landing/HeaderValidationTest.java
@@ -6,6 +6,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,24 +17,31 @@ public class HeaderValidationTest extends GsmartBaseTest {
 
 	public static final Logger logger = LogManager.getLogger(HeaderValidationTest.class);
 
+	@FindBy(linkText = "Shopping Center")
+	WebElement logoLink;
+	
+	@FindBy(linkText = "Exercise")
+	WebElement exerciseLink;
+	
+	@FindBy(linkText = "login")
+	WebElement loginLink;
+	
 	@Test
 	public void launchAndValidateLogoTest() throws IOException {
+		PageFactory.initElements(driver, this);
 		logger.info(" Launching the URL " + url);
 		driver.get(url);
 		logger.info(" validating Logo link ");
-		WebElement logoLink = driver.findElement(By.linkText("Shopping Centersssss"));
 		Assert.assertTrue(logoLink.isDisplayed(), "Logo link not displayed");
 
 	}
 
+	
 	@Test(dependsOnMethods = "launchAndValidateLogoTest")
 	public void validateExerciseAndLoginlinksTest() {
 		logger.info(" validating Exercise link ");
-		WebElement exerciseLink = driver.findElement(By.linkText("Exercise"));
 		Assert.assertTrue(exerciseLink.isDisplayed(), "Exercise link not displayed");
-
 		logger.info(" validating Login link ");
-		WebElement loginLink = driver.findElement(By.linkText("login"));
 		Assert.assertTrue(loginLink.isDisplayed(), "login link not displayed");
 
 	}
