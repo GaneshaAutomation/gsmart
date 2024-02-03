ada0875ccc3c8e5f13569d24c21d60b215bdb122
rshen <rshen@SangeetaPC>
2024-02-02 18:03:30 -0800

    GSM -Exercise and DropDown code

--- gsmart-test-automation/src/test/java/com/gsmart/test/automation/pages/exercise/ExercisePage.java
+++ gsmart-test-automation/src/test/java/com/gsmart/test/automation/pages/exercise/ExercisePage.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 import com.gsmart.test.automation.pages.base.BasePage;
 
@@ -16,20 +17,36 @@ public class ExercisePage extends BasePage {
 	// Actions, Navigations
 	@FindBy(xpath = "//button[contains(text(),'Cor')]")
 	WebElement corpInfo;
-	
+
 	@FindBy(xpath = " //button[contains(text(),\"SocialMedia\")]")
 	WebElement socialMedia;
+
+	@FindBy(id = "location")
+	WebElement cityDropDown;
+
+	public boolean isCityDropDownPresent() {
+		logger.info("Verifying Corp info tab");
+		boolean isPresent = cityDropDown.isDisplayed();
+		return isPresent;
+	}
 
 	public boolean isCorpInfoPresent() {
 		logger.info("Verifying Corp info tab");
 		boolean isPresent = corpInfo.isDisplayed();
 		return isPresent;
 	}
-	
+
 	public boolean isSocialMediaPresent() {
 		logger.info("Verifying Social Media Button");
 		boolean isPresent = socialMedia.isDisplayed();
 		return isPresent;
 	}
 
+	public void selectCityDd(String city) {
+
+		logger.info("Selecting City from Dropdown");
+		Select cityDropdown = new Select(cityDropDown);
+		cityDropdown.selectByVisibleText(city);
+
+	}
 }
---
+++ gsmart-test-automation/src/test/java/com/gsmart/test/automation/sksj/ExerciseAndCityDd.java
@@ -0,0 +1,37 @@
+package com.gsmart.test.automation.sksj;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.gsmart.test.automation.base.GsmartBaseTest;
+import com.gsmart.test.automation.pages.exercise.ExercisePage;
+import com.gsmart.test.automation.pages.home.HomePage;
+
+public class ExerciseAndCityDd extends GsmartBaseTest {
+
+	public static final Logger logger = LogManager.getLogger(ExerciseAndCityDd.class);
+
+	HomePage homePage;
+	ExercisePage exercisePage;
+
+	@Test
+	public void launchAndExerciseTabTest() {
+
+		driver.get(url);
+		homePage = new HomePage();
+		Assert.assertTrue(homePage.isMainHeaderPresent(), "Home page not loaded");
+		exercisePage = homePage.clickOnExercise();
+		Assert.assertTrue(exercisePage.isCorpInfoPresent(), "Exercise page not loaded");
+
+	}
+
+	@Test(dependsOnMethods = "launchAndExerciseTabTest")
+	public void CityDD() {
+
+		exercisePage.selectCityDd("Gilroy");
+
+	}
+
+}
